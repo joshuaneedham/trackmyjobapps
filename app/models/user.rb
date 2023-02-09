527d617ca08d010f63ea82d32126c514945ff3eb
Joshua Needham <joshuabneedham@gmail.com>
2023-02-08 19:10:35 -0600

    Fresh start

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:confirmable, :lockable, :timeoutable, :trackable
-
-  has_one_attached :profile_pic
-end
