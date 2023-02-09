2f616c9137f6b6c08ebb357bf301d69dbb7542b2
Joshua Needham <joshuabneedham@gmail.com>
2023-02-08 19:08:58 -0600

    Fresh tailwindcss Rails 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  root 'public#index'
-  devise_for :users
-  get 'public/about'
-  get 'public/contact'
-  get '/privacy', to: 'public#privacy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'public#index'
+  get 'public/about'
+  get 'public/contact'
+  get '/privacy', to: 'public#privacy'
 end
