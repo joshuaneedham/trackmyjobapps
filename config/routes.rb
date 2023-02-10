33f01ce4ff5814f4472104704fe2e17170047324
Joshua Needham <joshuabneedham@gmail.com>
2023-02-09 20:50:58 -0600

    Pushing up basic Rails 7 with simple controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'public#index'
+  get '/about-us', to: 'public#about'
+  get '/contact-us', to: 'public#contact'
+  
 end
